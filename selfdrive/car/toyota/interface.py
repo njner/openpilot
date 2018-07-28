297e05e4244152fc04e29526b66f59dba0b60eda
ErichMoraga <33645296+ErichMoraga@users.noreply.github.com>
2018-07-27 21:45:43 -0500

    Duplicated Prime settings to Prius
    This is just to see if the Prius has a positive effect, prior to 0.6

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -75,13 +75,13 @@ class CarInterface(object):
     if candidate == CAR.PRIUS:
       ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.70
-      ret.steerRatio = 15.59   # unknown end-to-end spec
-      tire_stiffness_factor = 0.7933
-      ret.mass = 3045 * CV.LB_TO_KG + std_cargo
-      ret.steerKpV, ret.steerKiV = [[0.4], [0.01]]
-      ret.steerKf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.steerRatio = 15.00   # unknown end-to-end spec
+      tire_stiffness_factor = 0.6371
+      ret.mass = 3370 * CV.LB_TO_KG + std_cargo
+      ret.steerKpV, ret.steerKiV = [[0.45], [0.015]]
+      ret.steerKf = 0.0000675   # full torque for 10 deg at 80mph means 0.00007818594
       # TODO: Prius seem to have very laggy actuators. Understand if it is lag or hysteresis
-      ret.steerActuatorDelay = 0.25
+      ret.steerActuatorDelay = 0.43
     elif candidate == CAR.PRIUS_PRIME:
       ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.70
